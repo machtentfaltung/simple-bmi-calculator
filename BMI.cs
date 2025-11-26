cce409fdee8105723b26b767ec510f6775b9526e
Matei Pralea <matei@pralea.me>
2025-11-26 20:19:51 +0200

    feat: initial commit

---
+++ BMI.cs
@@ -0,0 +1,11 @@
+// Copyright © 2024-2025 Matei Pralea <matei@pralea.me>
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+public class BMI
+{
+    public static float Calculate(float weight, float height)
+    {
+        return weight / (height * height);
+    }
+}
+
